6567d6f827b5636c045d279a4595d0d0c35be710
nuurosman <142992216+nuurosman@users.noreply.github.com>
2023-10-02 22:58:27 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    responsive:{
+        0:{
+          items: 1  
+        },
+        768:{
+            items: 2
+        },
+        1000:{
+            items: 3
+        }
+    }
+})
